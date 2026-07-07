15998f3f85c4a57eb7957989de70a0bd88155471
Abhishek Punia <abhishek004punia@gmail.com>
2026-07-07 12:47:35 +0530

    Configure Django with MySQL

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'hospital_db',
+        'USER': 'root',
+        'PASSWORD': 'Ajay@2004',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
