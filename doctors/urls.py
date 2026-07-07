fccad147a45bf6c2a3b15dff8bb3bd0acd164eab
Abhishek Punia <abhishek004punia@gmail.com>
2026-07-07 17:15:19 +0530

    Add doctor detail view page

--- doctors/urls.py
+++ doctors/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 
     path('add/', views.add_doctor, name='add_doctor'),
+    path('list/', views.doctor_list, name='doctor_list'),
+    path('view/<int:id>/', views.doctor_detail, name='doctor_detail'),
 
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Doctor
 from .forms import DoctorForm
 
 
@@ -18,4 +19,20 @@ def add_doctor(request):
 
     return render(request, 'doctors/add_doctor.html', {
         'form': form
+    })
+
+def doctor_list(request):
+
+    doctors = Doctor.objects.all()
+
+    return render(request, 'doctors/doctor_list.html', {
+        'doctors': doctors
+    })
+
+def doctor_detail(request, id):
+
+    doctor = get_object_or_404(Doctor, id=id)
+
+    return render(request, 'doctors/doctor_detail.html', {
+        'doctor': doctor
     })
