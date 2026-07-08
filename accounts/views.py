8b92c4d91196a8cd74aade0499d268b3784acdf9
Abhishek Punia <abhishek004punia@gmail.com>
2026-07-08 15:17:17 +0530

    Add live dashboard statistics

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 
+from patients.models import Patient
+from doctors.models import Doctor
+from appointments.models import Appointment
+
+
 def login_view(request):
     return render(request, 'accounts/login.html')
 
+
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+
+    total_patients = Patient.objects.count()
+
+    total_doctors = Doctor.objects.count()
+
+    total_appointments = Appointment.objects.count()
+
+    pending_appointments = Appointment.objects.filter(
+        status="Pending"
+    ).count()
+
+    context = {
+
+        "total_patients": total_patients,
+
+        "total_doctors": total_doctors,
+
+        "total_appointments": total_appointments,
+
+        "pending_appointments": pending_appointments,
+
+    }
+
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        context
+    )
