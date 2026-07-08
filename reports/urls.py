7d3f308c30a2fe7df76f02a56defee1454fc4363
Abhishek Punia <abhishek004punia@gmail.com>
2026-07-08 16:34:22 +0530

    Add reports dashboard

--- reports/urls.py
+++ reports/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
 
+    path(
+        "",
+        views.reports_dashboard,
+        name="reports_dashboard"
+    ),
+
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from patients.models import Patient
+from doctors.models import Doctor
+from appointments.models import Appointment
+from billing.models import Billing
+
+
+def reports_dashboard(request):
+
+    context = {
+
+        "total_patients": Patient.objects.count(),
+
+        "total_doctors": Doctor.objects.count(),
+
+        "total_appointments": Appointment.objects.count(),
+
+        "total_bills": Billing.objects.count(),
+
+    }
+
+    return render(
+        request,
+        "reports/dashboard_reports.html",
+        context
+    )
