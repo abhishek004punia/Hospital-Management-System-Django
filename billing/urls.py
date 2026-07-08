52ace9342f56649de7fe5c887234bd6cf9df406d
Abhishek Punia <abhishek004punia@gmail.com>
2026-07-08 15:54:56 +0530

    Add bill detail view

--- billing/urls.py
+++ billing/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("add/", views.add_bill, name="add_bill"),
-     path("list/", views.bill_list, name="bill_list"),
+    path("list/", views.bill_list, name="bill_list"),
+    path("view/<int:id>/", views.bill_detail, name="bill_detail"),
 ]
--- billing/views.py
+++ billing/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BillingForm
 from .models import Billing
 
@@ -37,5 +37,19 @@ def bill_list(request):
         "billing/bill_list.html",
         {
             "bills": bills
+        }
+    )
+def bill_detail(request, id):
+
+    bill = get_object_or_404(
+        Billing,
+        id=id
+    )
+
+    return render(
+        request,
+        "billing/bill_detail.html",
+        {
+            "bill": bill
         }
     )
